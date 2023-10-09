04b9ef3d9464b78679401afcbae13ae20e27c5fa
mixverse-master <metaversehash@gmail.com>
2023-10-09 19:44:41 +0800

    Update display_server_visionos.h

--- platform/visionos/display_server_visionos.h
+++ platform/visionos/display_server_visionos.h
@@ -18,4 +18,24 @@
 #endif // VULKAN_ENABLED
 
 
+class DisplayServerVISIONOS : public DisplayServer {
+    // No need to register with GDCLASS, it's platform-specific and nothing is added.
+
+	_THREAD_SAFE_CLASS_
+
+#if defined(VULKAN_ENABLED)
+	VulkanContextVISIONOS *context_vulkan = nullptr;
+	RenderingDeviceVulkan *rendering_device_vulkan = nullptr;
+#endif
+
+    DisplayServerVISIONOS(const String &p_rendering_driver, DisplayServer::WindowMode p_mode, DisplayServer::VSyncMode p_vsync_mode, uint32_t p_flags, const Vector2i *p_position, const Vector2i &p_resolution, int p_screen, Error &r_error);
+	~DisplayServerVISIONOS();
+
+public:
+	String rendering_driver;
+
+	static DisplayServerIOS *get_singleton();
+
+};
+
 #endif // DISPLAY_SERVER_VISIONOS_H
